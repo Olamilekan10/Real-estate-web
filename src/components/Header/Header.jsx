6b9182584d77130af306ab223c6c1e0f52dfb053
olamilekan <issaabubakry@gmail.com>
2023-08-03 22:10:10 +0100

    imported logo and created more div sections

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,14 @@ import React from 'react'
 const Header = () => {
   return (
     <section className="h-wrapper">
-      
+      <div className="h-container">
+
+        <img src="./logo.png" alt="logo" width={100} />
+
+        <div className="h-menu">
+          
+        </div>
+      </div>
     </section>
   )
 }
