0d832b37eb0414fff40c1500947872c50edc5386
olamilekan <issaabubakry@gmail.com>
2023-08-04 15:37:18 +0100

    created a section in the second component

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Hero = () => {
+  return (
+    <section className="hero-wrapper">
+        <div className="paddings innerWidth flexCenter hero-container">
+
+            <div className="hero-left">
+                Left section
+            </div>
+
+            <div className="flexCenter hero-left">
+                <div className="image-container">
+                    <img src="./hero-image.png" alt="" />
+                </div>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default Hero;
