86c031ffcc2413c9393ac435c2734106fc0defe2
olamilekan <issaabubakry@gmail.com>
2023-08-04 22:13:42 +0100

    figured how to align the header properly

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -10,7 +10,7 @@ const Hero = () => {
             <div className="hero-left">
 
                 <div className="hero-title">
-                    <h1>Discover Most Suitable Property</h1>
+                    <h1>Discover <br /> Most Suitable <br /> Property</h1>
                 </div>
             </div>
 
