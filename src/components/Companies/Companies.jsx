9aadd6d806a0190d3da958964639c65b28063619
olamilekan <issaabubakry@gmail.com>
2023-08-07 21:57:58 +0100

    created img tag with images inserted

--- src/components/Companies/Companies.jsx
+++ src/components/Companies/Companies.jsx
@@ -5,10 +5,10 @@ const Companies = () => {
   return (
     <section className="c-wrapper">
         <div className="paddings innerWidth flexCenter c-container">
-            <img src="" alt="" />
-            <img src="" alt="" />
-            <img src="" alt="" />
-            <img src="" alt="" />
+            <img src="./prologis.png" alt="" />
+            <img src="./tower.png" alt="" />
+            <img src="./equinix.png" alt="" />
+            <img src="./realty.png" alt="" />
         </div>
     </section>
   )
