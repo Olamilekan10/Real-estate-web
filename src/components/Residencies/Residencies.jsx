0c2a5437b34b47e6ebd5313718eaaae3d828d816
olamilekan <issaabubakry@gmail.com>
2023-08-07 22:17:57 +0100

    imported swiper from react and linked css file to comp

--- src/components/Residencies/Residencies.jsx
+++ src/components/Residencies/Residencies.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import {Swiper, SwiperSlide, useSwiper} from 'swiper/react';
+import "swiper/css";
+import './Residencies.css';
 
 const Residencies = () => {
   return (
