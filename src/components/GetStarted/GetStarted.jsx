6485b810dfd428cff2da8ccafe780d08d133d42e
olamilekan <issaabubakry@gmail.com>
2023-08-12 12:53:41 +0100

    created some spaan tag and in it some content

--- src/components/GetStarted/GetStarted.jsx
+++ src/components/GetStarted/GetStarted.jsx
@@ -6,7 +6,12 @@ const GetStarted = () => {
     <section className="g-wrapper">
         <div className="paddings innerWidth g-container">
             <div className="flexColCenter inner-container">
-                
+                <span>Get Started with Homyz</span>
+                <span>
+                    Subscribe and find super attractive price quotes from 
+                    <br />
+                    Find your residence soon
+                </span>
             </div>
         </div>
     </section>
