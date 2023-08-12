a11183fc53ca1cb1e9aa83ad59660c83fd309ca1
olamilekan <issaabubakry@gmail.com>
2023-08-12 15:27:04 +0100

    split section into two - started with left side

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,7 +5,11 @@ const Footer = () => {
   return (
     <section className="f-wrapper">
         <div className="paddings innerWidth flexCenter f-container">
-            
+
+            {/* left side */}
+            <div className="flexColStart f-left">
+                <img src="./logo2.png" alt=""  />
+            </div>
         </div>
     </section>
   )
