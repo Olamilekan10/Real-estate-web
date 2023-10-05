e83ef1b81d07234912848105a18425b10889b8a0
olamilekan <issaabubakry@gmail.com>
2023-10-05 13:19:36 +0100

    make use of the imported handler as a abbreviation wrapped around the menu option

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -18,17 +18,19 @@ const Header = () => {
 
         <img src="./logo.png" alt="logo" width={100} />
 
-        <div className="flexCenter h-menu"
-          style={getMenuStyles(menuOpened)}
-        >
-          <a href="">Residencies</a>
-          <a href="">Our Value</a>
-          <a href="">Contact Us</a>
-          <a href="">Get Started</a>
-          <button className="button">
-          <a href="">Contact</a>
-          </button>
-        </div>
+        <OutsideClickHandler>
+          <div className="flexCenter h-menu"
+            style={getMenuStyles(menuOpened)}
+            >
+            <a href="">Residencies</a>
+            <a href="">Our Value</a>
+            <a href="">Contact Us</a>
+            <a href="">Get Started</a>
+            <button className="button">
+            <a href="">Contact</a>
+            </button>
+          </div>
+        </OutsideClickHandler>
 
         <div className="menu-icon"  onClick={()=> setMenuOpened((prev)=>!prev)} >
           <BiMenuAltRight size={30} />
