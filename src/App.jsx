f5b2de624617d86532121e92fb540a89036f5d31
olamilekan <issaabubakry@gmail.com>
2023-10-05 13:36:46 +0100

    now, created a media screen for the hero component

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
       <div>
         <div className="white-gradient" />
         <Header />
-        {/* <Hero /> */}
+        <Hero />
       </div>
       {/* <Companies />
       <Residencies />
