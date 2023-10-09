c4fae2f1954a3eccc10d068ea08f7bdf241827a2
olamilekan <issaabubakry@gmail.com>
2023-10-10 00:15:57 +0100

    now, it's perfect and working - ends the projects

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -77,7 +77,7 @@ const Hero = () => {
                     animate={{x: 0, opacity: 1}}
                     transition={{
                         duration: 2,
-                        type: "spring"
+                        // type: "spring"
                     }}
                     className="image-container">
                     <img src="./hero-image.png" alt="" />
